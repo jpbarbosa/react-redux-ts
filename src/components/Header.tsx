f5395d5a85f03cba70a085c5362317883e7c1186
JP Barbosa <jp@jp7.com.br>
2021-06-22 18:06:24 -0300

    Show total number of tasks

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { StoreState } from '../types/store';
+import { TasksState } from '../types/tasks';
 
 export const Header: React.FC = () => {
+  const tasks = useSelector<StoreState, TasksState>((state) => state.tasks);
+
   return (
     <header>
       <h1>
         <div className="title">React Basic CRUD</div>
         <div className="subtitle">With Redux And TypeScript</div>
       </h1>
-      <div className="counter">0</div>
+      <div className="counter">{tasks.all.length}</div>
     </header>
   );
 };
