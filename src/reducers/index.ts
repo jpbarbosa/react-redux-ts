75fabd15d2521f52c81ebc846db11bd8cdba338e
JP Barbosa <jp@jp7.com.br>
2021-06-25 14:26:29 -0300

    Add type to combineReducers

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
+import { StoreState } from '../types/store';
 import { tasks } from './tasks';
 
-export const reducers = combineReducers({
+export const reducers = combineReducers<StoreState>({
   tasks,
 });
